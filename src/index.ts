44d84424369bcbe9e9609f356f9d3905f6aa0bbc
RinkeshGolwala <35759871+RinkeshGolwala@users.noreply.github.com>
2025-03-31 09:01:45 +0000

    [TDD-ADD]: Implement logic for comma-separated numbers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 export function add(numbers: string): number {
+    let defaultDelimiter = ',';
+
     // base case
     if (numbers === '') {
         return 0;
     }
+
+    const result = numbers.split(defaultDelimiter).reduce((total, num) => total + Number(num), 0);
+
+    return result;
 }
